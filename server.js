c6b3112ffbfdc62566fa0c4dc2f3048adbe1c2e4
camyacodes <camyacodes@gmail.com>
2022-01-10 17:48:04 -0500

    created server that displays notes.json

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const notes = require('./data/notes.json');
+
+const express = require('express');
+
+const app = express();
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+  
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+  
