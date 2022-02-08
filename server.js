b88e6b01ac02d101f070c1d9632158f7930a686e
camyacodes <camyacodes@gmail.com>
2022-02-08 18:58:40 -0500

    fixed directory paths to get the app working

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
-const notes = require("./data/notes.json");
+const fs = require("fs");
+const path = require("path");
 
 const express = require("express");
 
@@ -14,16 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/api/notes", (req, res) => {
+	const notes = readNotes();
 	res.json(notes);
 });
 
 app.post("/api/notes", (req, res) => {
+	const notes = readNotes();
+	req.body.id = notes.length.toString();
 	// req.body is where our incoming content will be
 	console.log(req.body);
 
+	console.log(notes);
 	const note = createNewNote(req.body, notes);
 
 	res.json(note);
@@ -31,23 +33,30 @@ app.post("/api/notes", (req, res) => {
 
 function createNewNote(body, notesArray) {
 	const note = body;
-    notesArray.push(note);
-    
-    fs.writeFileSync(
-        path.join(__dirname, './data/notes.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-      );
+	notesArray.push(note);
+
+	fs.writeFileSync(
+		path.join(__dirname, "./data/notes.json"),
+		JSON.stringify(notesArray, null, 2)
+	);
 
 	return note;
 }
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+function readNotes() {
+	const notes = JSON.parse(
+		fs.readFileSync(path.join(__dirname, "./data/notes.json"))
+	);
+	return notes;
+}
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
 });
-  
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+
+app.get("/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`API server now on port ${PORT}!`);
